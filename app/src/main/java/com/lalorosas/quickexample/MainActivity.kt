3abd570db4a0099127043b8fcf54e2ea9dcecda0
LaloCo <lalorosas@outlook.com>
2019-04-10 14:57:36 -0500

    Initial commit

---
+++ app/src/main/java/com/lalorosas/quickexample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.lalorosas.quickexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        helloButton.setOnClickListener { v ->
+            helloText.text = "Hello ".plus(nameEditText.text.toString()).plus("!");
+        }
+    }
+}
